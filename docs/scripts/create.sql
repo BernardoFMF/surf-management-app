f08a75d0ead1e97a46513e70cd11818d604327a7
Gonçalo Albuquerque <gjca01@hotmail.com>
2022-04-05 18:05:49 +0100

    Create and drop tables

---
+++ docs/scripts/create.sql
@@ -0,0 +1,131 @@
+create table Candidate_ (
+	id_ 			serial,
+	nif_			int,
+	cc_ 			varchar(30),
+	full_name_ 		varchar(60),
+	nationality_ 	varchar(30),
+	birth_date_		varchar(30),
+	location_		varchar(30),
+	address_ 		varchar(40),
+	postal_code_ 	varchar(20) check (postal_code_ like '%-%'),
+	email_ 			varchar(30) check (email_ like '%@%'),
+	phone_number_	int,
+	pword_			varchar(100),
+	
+	primary key(id_)
+);
+
+create table Member_ (
+	id_ 			serial,
+	member_type_	varchar(40) check (member_type_ in ('founder', 'effective', 'merit', 'corporate')),
+	has_debt_ 		bool,
+	quota_value_ 	int,
+	
+	primary key(id_)
+);
+
+create table Event_ (
+	id_ 			serial,
+	name_ 			varchar(50),
+	initial_date_	date,
+	end_date_ 		date,
+	
+	primary key(id_)
+		
+);
+
+create table Attendance_ (
+	member_id_ 	 	int,
+	event_id_		int,
+	state_			varchar(20) check (state_ in ('going', 'not going', 'interested')),
+	
+	primary key (member_id_, event_id_),
+	constraint fk_member foreign key(member_id_) references Member_(id_),
+	constraint fk_event foreign key(event_id_) references Event_(id_)
+);
+
+create table Quota_ (
+	id_ 	 		serial,
+	member_id_ 	 	int,
+	amount_			int,
+	payment_date_	date,
+	date_			date,
+	
+	primary key (id_),
+	constraint fk_member foreign key(member_id_) references Member_(id_)
+);
+
+create table Contact_ (
+	member_id_ 	 	int,
+	location_		varchar(30),
+	address_ 		varchar(40),
+	postal_code_ 	varchar(20) check (postal_code_ like '%-%'),
+	email_ 			varchar(30) check (email_ like '%@%'),
+	phone_number_	int,
+	
+	primary key (member_id_),
+	constraint fk_member foreign key(member_id_) references Member_(id_)
+);
+
+create table Company_ (
+	member_id_ 	 	int,
+	nif_			int,
+	name_ 			varchar(40),
+
+	primary key (member_id_),
+	constraint fk_member foreign key(member_id_) references Member_(id_)
+);
+
+create table User_ (
+	member_id_ 	 	int,
+	nif_			int,
+	cc_ 			varchar(30),
+	full_name_ 		varchar(60),
+	nationality_ 	varchar(30),
+	birth_date_		varchar(30),
+	enrollment_date_	date,
+	paid_enrollment_	bool,
+	pword_			varchar(100),
+
+	primary key (member_id_),
+	constraint fk_member foreign key(member_id_) references Member_(id_)
+);
+
+create table User_Img_ (
+	user_id_ 	 	int,
+	img_name_		varchar(30),
+	img_value 		bytea,
+
+	primary key (user_id_),
+	constraint fk_user foreign key(user_id_) references User_(member_id_)
+);
+
+create table Sport_ (
+	id_ 	 		serial,
+	name_			varchar(30),
+
+	primary key (id_)
+);
+
+create table User_Sport_ (
+	user_id_ 	 	int,
+	sport_id_ 	 	int,
+
+	primary key (user_id_, sport_id_),
+	constraint fk_user foreign key(user_id_) references User_(member_id_),
+	constraint fk_sport foreign key(sport_id_) references Sport_(id_)
+);
+
+create table Federation_Info_ (
+	id_ 	 		serial,
+	fed_id_			int,
+	name_			varchar(30),
+	years_federated int,
+	user_id_ 	 	int,
+
+	primary key (id_),
+	constraint fk_user foreign key(user_id_) references User_(member_id_)
+);
+
+
+
---
+++ docs/scripts/drop.sql
@@ -0,0 +1,12 @@
+drop table if exists Federation_Info_;
+drop table if exists User_Sport_;
+drop table if exists Sport_;
+drop table if exists User_Img_;
+drop table if exists User_;
+drop table if exists Company_;
+drop table if exists Contact_;
+drop table if exists Quota_;
+drop table if exists Attendance_;
+drop table if exists Event_;
+drop table if exists Member_;
+drop table if exists Candidate_;
