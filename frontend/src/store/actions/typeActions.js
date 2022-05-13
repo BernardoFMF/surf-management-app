44fda83f6dcf96645bcb2c0329e5f7e4e873f9dd
Gonçalo Albuquerque <gjca01@hotmail.com>
2022-05-13 15:23:54 +0100

    Finished management quota page & added social to home page

--- frontend/src/store/actions/typeActions.js
+++ frontend/src/store/actions/typeActions.js
@@ -1,4 +1,4 @@
-import { TYPES_FETCH_SUCCESS, TYPES_FETCH_REQUEST, TYPES_FETCH_FAIL, TYPES_UPDATE_REQUEST, TYPES_UPDATE_SUCCESS, TYPES_UPDATE_FAIL } from '../constants/typeConstants'
+import { TYPES_FETCH_SUCCESS, TYPES_FETCH_REQUEST, TYPES_FETCH_FAIL, TYPES_UPDATE_REQUEST, TYPES_UPDATE_SUCCESS, TYPES_UPDATE_FAIL, TYPES_CREATE_REQUEST, TYPES_CREATE_SUCCESS, TYPES_CREATE_FAIL } from '../constants/typeConstants'
 
 export const getTypes = () => async (dispatch) => {
     try {
@@ -47,6 +47,36 @@ export const updateTypes = (type, quota_value) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: TYPES_UPDATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const createType = (type, quota_value) => async (dispatch) => {
+  try {
+    dispatch({
+      type: TYPES_CREATE_REQUEST,
+    })
+    const response = await fetch(`/api/quotas/management`, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({"type": type, "quota_value": quota_value})
+    })
+    console.log(response);
+    const types = await response.json()
+    console.log(types);
+    if(response.status !== 201) throw Error(types.message_code)
+    dispatch({
+      type: TYPES_CREATE_SUCCESS,
+      payload: types,
+    })
+
+  } catch (error) {
+    dispatch({
+      type: TYPES_CREATE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
