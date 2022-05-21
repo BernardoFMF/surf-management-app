4fd6a490b7d331cc0851a55d5fb12161c666953a
Miguel <A47270@alunos.isel.pt>
2022-05-21 18:33:34 +0100

    companies paging and filtering

--- frontend/src/Pages/application/AllCompaniesPage.jsx
+++ frontend/src/Pages/application/AllCompaniesPage.jsx
@@ -3,27 +3,58 @@ import { useDispatch, useSelector } from 'react-redux'
 import { deleteCompany, getCompanies } from '../../store/actions/companyActions'
 import BusinessIcon from '@mui/icons-material/Business';
 import {  Stack, CircularProgress} from '@mui/material'
+import { getTypes } from '../../store/actions/typeActions'
 import { useTranslation } from 'react-i18next'
 import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
 import DeleteIcon from '@mui/icons-material/Delete';
 import MainCard from '../../components/cards/MainCard';
-
+import { Form, Formik } from 'formik';
+import InputField from '../../components/multiStepForm/InputField';
+import Box from '@mui/material/Box';
+import { Grid, Alert} from '@mui/material'
+import AnimateButton from '../../components/extended/AnimateButton'
+import LoadingButton from '@mui/lab/LoadingButton'
+import SearchIcon from '@mui/icons-material/Search';
+import { useNavigate } from 'react-router';
+import { Pagination } from '@mui/material';
 
 const AllCompaniesPage = () => {
 
     const {t} = useTranslation()
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     const companiesFetch = useSelector((state) => state.companiesFetch)
-    const { loading, companiesGet } = companiesFetch
+    const { loading, error, companiesGet } = companiesFetch
+
     const [rows, setRows] = useState([]);
-    
+    const [open, setOpen] = React.useState(false);
+    const handleClose = () => setOpen(false);
+
+    const [ searchState, setSearchState ] = useState({
+      username_filter: "",
+      name_filter: "",
+      email_filter: ""
+  })
+
+  const typesFetch = useSelector((state) => state.typesFetch)
+    const { loading: loadingTypes, error: errorTypes, typesGet } = typesFetch
+  
+  const [page, setPage] = useState(1);
+  const limit = 5
+
     useEffect(() => {
-        dispatch(getCompanies())
-    },[])
+        dispatch(getCompanies(searchState.username_filter, searchState.name_filter, searchState.email_filter, 0, limit))
+        dispatch(getTypes())
+    }, [])
+
+  const changePageHandler = (event, value) => {
+    setPage(value)
+    dispatch(getCompanies(searchState.username_filter, searchState.name_filter, searchState.email_filter, (value-1)*limit, limit))
+}
 
     useEffect(() => {
         if(companiesGet){
-            setRows(companiesGet.map(company => {
+            setRows(companiesGet.companies.map(company => {
                 let x = {
                     ...company, id: company.member_id_
                 }
@@ -43,16 +74,12 @@ const AllCompaniesPage = () => {
       [],
     );
   
-    const showProfileHandle = React.useCallback(
-      (id) => () => {
-        setRows((prevRows) =>
-          prevRows.map((row) =>
-            row.id === id ? { ...row, isAdmin: !row.isAdmin } : row,
-          ),
-        );
-      },
-      [],
-    );
+    const searchHandler = async(values) => {
+      setSearchState(values)
+      setPage(1)
+      setRows([])
+      dispatch(getCompanies(values.username_filter,values.name_filter,values.email_filter,0,limit))
+  }
 
 const columns = [
     { field: 'member_id_', headerName: 'ID', width: 40 },
@@ -67,11 +94,11 @@ const columns = [
         type: 'actions',
         width: 110,
         getActions: (params) => [
-            <GridActionsCellItem
-            icon={<BusinessIcon />}
-            label="Show Profile"
-            onClick={showProfileHandle(params.id)}
-            />,
+          <GridActionsCellItem
+          icon={<BusinessIcon />}
+          label="Show Profile"
+          onClick={() => navigate(`/application/members/${params.id}`)}
+          />,
             <GridActionsCellItem
             icon={<DeleteIcon />}
             label="Delete"
@@ -86,20 +113,65 @@ const columns = [
   return (
     <>
       <MainCard title={t('companies')}sx={{height: '100%'}}>
+      { error && <Box sx={{ pl: { md: 2 }, pt: 2 }}><Alert severity="error">{t(error)}</Alert></Box> }
+            { errorTypes && <Box sx={{ pl: { md: 2 }, pt: 2 }}><Alert severity="error">{t(errorTypes)}</Alert></Box> }
+            <Formik
+                    initialValues={searchState}
+                    enableReinitialize={true}
+                    onSubmit={values => searchHandler(values)}
+                >
+                {formik => (
+                    <Form>
+                        <Grid container spacing={2} direction="row" alignItems={'center'} sx={{ mb: 2}}>
+                            <Grid item>
+                                <InputField name='username_filter' label={t('sign_up_username')} type='text'></InputField>
+                            </Grid>
+                            <Grid item>
+                                <InputField name='name_filter' label={t('sign_up_full_name')} type='text' ></InputField>
+                            </Grid>
+                            <Grid item>
+                                <InputField name='email_filter' label={t('sign_up_email')} type='text' ></InputField>
+                            </Grid>
+                            <Grid item>
+                                <AnimateButton>
+                                    <LoadingButton
+                                        disableElevation
+                                        size="large"
+                                        type="submit"
+                                        variant="contained"
+                                        color="primary"
+                                        loading = {loading}
+                                        startIcon={<SearchIcon></SearchIcon>}
+                                    >
+                                        {t('Search')}
+                                    </LoadingButton>
+                                </AnimateButton>
+                            </Grid>    
+                        </Grid>
+                    </Form>
+                )}
+            </Formik>
       { loading ? 
         <Stack alignItems="center">
             <CircularProgress size='4rem'/>
-        </Stack> :
+        </Stack> : (
+        <>
         <DataGrid
-          autoHeight
-          rows={rows}
-          columns={columns}
-          pageSize={10}
-          rowsPerPageOptions={[10]}
-          checkboxSelection
-          experimentalFeatures={{ newEditingApi: true }}
-        /> 
-      }
+            autoHeight
+            rows={rows}
+            columns={columns}
+            pageSize={limit}
+            hideFooter={true}
+            onPageChange={changePageHandler}
+            sx={{
+                "& .MuiDataGrid-columnHeaders": {
+                    backgroundColor: "rgba(219, 219, 219, 0.5)"
+                }
+            }}
+        />
+        <Pagination sx={{ mt: 2 }} variant="outlined" shape='rounded' color="primary" count={Math.ceil(companiesGet.number_of_companies / limit)} page={page} onChange={changePageHandler} showFirstButton showLastButton/>
+        </>
+      )}
       </MainCard> 
     </>
   )
