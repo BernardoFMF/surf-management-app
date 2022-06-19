8330acd67568db2f4013e3a6604149be5f8503e5
Miguel <A47270@alunos.isel.pt>
2022-06-19 10:23:01 +0100

    change variable

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config()
 
 //import data from './data/__mock__/mockDataAccess.js'
 import db from './data/__db__/dbDataAccess.js'
-const data = db(process.env.PG_USER, process.env.PG_PASSWORD, process.env.PG_HOST, process.env.PG_PORT, process.env.PG_DB_TEST, process.env.NODE_ENV)
+const data = db(process.env.PG_USER, process.env.PG_PASSWORD, process.env.PG_HOST, process.env.PG_PORT, process.env.PG_DB, process.env.NODE_ENV)
 
 const app = express()
 
